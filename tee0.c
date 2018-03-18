612f32fd8b7ca9a00c20862669ec492e7fc9fce6
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-18 12:51:29 -0300

    add tee0

---
+++ tee0.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 256
+
+
+int main(int argc, char** argv) {
+    int fd = open(argv[1], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (fd < 0) {
+        return 1;
+    }
+
+    ssize_t bytes = 0;
+    do {
+        char buf[BUF_SIZE] = {0};
+        bytes = read(0, buf, BUF_SIZE);
+        write(1, buf, BUF_SIZE);
+        write(fd, buf, BUF_SIZE);
+    } while (bytes);
+
+    close(fd);
+    return 0;
+}
+

20a1d1ce34e7d7f2db7bfd63ce966a5cb4ec3475
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-25 19:42:49 -0300

    add move0

---
+++ mv0.c
@@ -0,0 +1,16 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+
+/*
+ * Pregunta: ¿se puede usar mv0 para renombrar archivos dentro del mismo directorio?
+ * Sí, usar mv es la "forma unix" de renombrar archivos
+ */
+
+void mv0(const char* oldpath, const char* newpath) {
+    rename(oldpath, newpath);
+}
+
+int main(int argc, const char** argv) {
+    mv0(argv[1], argv[2]);
+}

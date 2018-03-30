aa5c5882ec1acbea7f080db8ab63de62182d5dbe
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-30 00:51:07 -0300

    Challenge: Agrego status de cada pid

--- ps0.c
+++ ps0.c
@@ -41,15 +41,35 @@ void ps0() {
         char comm[17] = {0};
         snprintf(comm, 17, "/proc/%s/comm", ent->d_name);
 
-        int fd = open(comm, O_RDONLY);
-        if (fd < 0) {
+        int comm_fd = open(comm, O_RDONLY);
+        if (comm_fd < 0) {
             perror("Error opening process comm file");
             return;
         }
-        char proc_name[256] = {0};
-        read(fd, proc_name, 256);
 
-        printf("%s %s", ent->d_name, proc_name);
+        char proc_name[256] = {0};
+        read(comm_fd, proc_name, 256);
+
+        char stat[17] = {0};
+        snprintf(stat, 17, "/proc/%s/stat", ent->d_name);
+
+        int stat_fd = open(stat, O_RDONLY);
+        if (stat_fd < 0) {
+            perror("Error opening process stat file");
+            return;
+        }
+
+        char stat_content[1024] = {0};
+        read(stat_fd, stat_content, 1024);
+        char status;
+        int matched = sscanf(stat_content, "%*s %*s %c", &status);
+
+        if (matched != 1) {
+            printf("Error running sscanf on stat file\n");
+            return;
+        }
+
+        printf("%s %c %s", ent->d_name, status, proc_name);
         ent = readdir(proc_root);
     }
 }

3f24bf11468fb9df0f492d92df96d10dce96f025
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-31 20:00:49 -0300

    Emprolijo ln0

--- ln0.c
+++ ln0.c
@@ -1,5 +1,6 @@
 #define _POSIX_C_SOURCE 200809L
 #include <unistd.h>
+#include <stdio.h>
 
 
 /*
@@ -10,7 +11,10 @@
  */
 
 void ln0(const char* from, const char* to) {
-    symlink(from, to);
+    if (symlink(from, to) < -1) {
+        perror("Error creando el symlink");
+        return;
+    }
 }
 
 int main(int argc, const char** argv) {

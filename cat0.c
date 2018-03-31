abfd2c3fd1b95a7de9845c757f0adf2e0c944301
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-31 17:58:26 -0300

    Validaciones a cat0

--- cat0.c
+++ cat0.c
@@ -1,19 +1,30 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 #define BUF_SIZE 256
 
 void cat0(const char* file) {
-    int fd = open(argv[1], O_RDONLY);
+    int fd = open(file, O_RDONLY);
     if (fd < 0) {
-        return 1;
+        return;
     }
 
     ssize_t bytes = 0;
     do {
         char buf[BUF_SIZE] = {0};
         bytes = read(fd, buf, BUF_SIZE);
-        write(1, buf, BUF_SIZE);
+
+        if (bytes < 0) {
+            perror("Error leyendo del archivo de entrada");
+            return;
+        }
+
+        if (write(STDOUT_FILENO, buf, BUF_SIZE) < 0) {
+            perror("Error escribiendo a salida estándar");
+            return;
+        }
+
     } while (bytes == BUF_SIZE);
 
     close(fd);
@@ -23,4 +34,3 @@ int main(int argc, char** argv) {
     cat0(argv[1]);
     return 0;
 }
-

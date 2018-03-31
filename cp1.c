254bb453a06e89a130e3fca3d07a146b5f35673b
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-31 18:58:41 -0300

    Emprolijo cp1

--- cp1.c
+++ cp1.c
@@ -1,30 +1,36 @@
+#define _POSIX_C_SOURCE 200112L
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 void cp1(const char* src, const char* dst) {
 
     size_t page_size = (size_t) sysconf(_SC_PAGESIZE);
-    int fd = open(src, O_RDWR);
-
-    if (fd < 0) {
-        perror("Error opening src");
+    if (page_size == -1 && errno == EINVAL) {
+        perror("Error obteniendo el tamaño de página del sistema");
         return;
     }
 
-    int o_fd = open(dst, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (o_fd < 0) {
-        perror("Error opening dst");
+    int src_fd = open(src, O_RDWR);
+    if (src_fd < 0) {
+        perror("Error abriendo archivo de entrada");
+        return;
+    }
+
+    int dst_fd = open(dst, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (dst_fd < 0) {
+        perror("Error creando archivo destino");
         return;
     }
 
     size_t offset = 0;
     struct stat s;
-    if (fstat(fd, &s) < 0) {
-        perror("Error getting stat of fd");
+    if (fstat(src_fd, &s) < 0) {
+        perror("Error en stat del archivo de entrada");
         return;
     }
 
@@ -35,32 +41,51 @@ void cp1(const char* src, const char* dst) {
             bytes_to_write = remaining;
         }
 
-        void *map_src = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
+        void *map_src = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, src_fd, offset);
         if (map_src == MAP_FAILED) {
-            perror("Error mapping src");
+            perror("Error mappeando src");
             return;
         }
 
-        if (ftruncate(o_fd, offset + bytes_to_write)) {
-            perror("Error seeking to end of o_fd");
+        if (ftruncate(dst_fd, offset + bytes_to_write)) {
+            perror("Error truncando dst");
             return;
         }
 
-        void *mmap_dst = mmap(NULL, bytes_to_write, PROT_READ | PROT_WRITE, MAP_SHARED, o_fd, offset);
+        void *mmap_dst = mmap(NULL, bytes_to_write, PROT_READ | PROT_WRITE, MAP_SHARED, dst_fd, offset);
         if (mmap_dst == MAP_FAILED) {
-            perror("Error mapping dst");
+            perror("Error mappeando dst");
             return;
         }
 
-        memcpy(mmap_dst, map_src, bytes_to_write);
-        munmap(mmap_dst, bytes_to_write);
-        munmap(map_src, bytes_to_write);
+        if (!memcpy(mmap_dst, map_src, bytes_to_write)) {
+            perror("Error copiando memoria de src a dst");
+            return;
+        }
+
+        if (munmap(mmap_dst, bytes_to_write) < 0) {
+            perror("Error en el unmap de dst");
+            return;
+        }
+
+        if (munmap(map_src, bytes_to_write) < 0) {
+            perror("Error en el unmap de src");
+            return;
+        }
+
         offset += bytes_to_write;
         remaining -= bytes_to_write;
     }
 
-    close(fd);
-    close(o_fd);
+    if (close(src_fd) < 0) {
+        perror("Error cerrando el archivo de entrada");
+        return;
+    }
+
+    if (close(dst_fd) < 0) {
+        perror("Error cerrando el archivo destino");
+        return;
+    }
 }
 
 int main(int argc, char** argv) {

f0fb529693d988ec0beb32c6cd27f9032a024dfb
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-31 19:58:34 -0300

    Emprolijo ls0

--- ls0.c
+++ ls0.c
@@ -1,9 +1,17 @@
 #include <dirent.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
 
 void ls0() {
     DIR* dir = opendir(".");
 
+    if (!dir) {
+        perror("Error abriendo el directorio");
+        return;
+    }
+
+    errno = 0;
     struct dirent *dirent = readdir(dir);
     while(dirent) {
 
@@ -15,11 +23,23 @@ void ls0() {
             c = dirent->d_name[bytes];
         }
 
-        write(1, dirent->d_name, bytes);
+        if (write(STDOUT_FILENO, dirent->d_name, bytes) < 0) {
+            perror("Error escribiendo el nombre de archivo");
+            return;
+        }
         write(1, "\n", 1);
         dirent = readdir(dir);
     }
-    closedir(dir);
+
+    if (errno) {
+        perror("Error leyendo el directorio");
+        return;
+    }
+
+    if (closedir(dir) < 0) {
+        perror("Error cerrando el directorio");
+        return;
+    }
 }
 
 int main() {

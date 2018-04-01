502e0c0b11503a8c5eab04b714707e9c93f6d6ca
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-31 22:13:41 -0300

    touch: validate

--- touch0.c
+++ touch0.c
@@ -3,11 +3,18 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 
 void touch0(const char* file) {
     int fd = open(file, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    close(fd);
+    if (fd < 0) {
+        perror("Error abriendo el archivo de entrada");
+    }
+
+    if (close(fd) < 0) {
+        perror("Error cerrando el archivo de entrada");
+    }
 }
 
 int main(int argc, char** argv) {
--- touch1.c
+++ touch1.c
@@ -2,11 +2,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <utime.h>
+#include <stdio.h>
 
-void touch1(const char* filename) {
-    if (utime(filename, NULL) < 0) {
-        int fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-        close(fd);
+void touch1(const char* file) {
+    if (utime(file, NULL) < 0) {
+        int fd = open(file, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+        if (fd < 0) {
+            perror("Error abriendo el archivo de entrada");
+        }
+
+        if (close(fd) < 0) {
+            perror("Error cerrando el archivo de entrada");
+        }
     };
 
 }

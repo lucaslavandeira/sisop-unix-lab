3051e8655d552736dd5eee458825e2ca5d999b71
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-31 22:11:25 -0300

    tee0: validaciones

--- tee0.c
+++ tee0.c
@@ -1,27 +1,45 @@
+#define _POSIX_C_SOURCE 200809L
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 #define BUF_SIZE 256
 
 
 void tee0(const char* file) {
-    int fd = open(argv[1], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    int fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
     if (fd < 0) {
-        return 1;
+        return;
     }
 
     ssize_t bytes = 0;
     do {
         char buf[BUF_SIZE] = {0};
-        bytes = read(0, buf, BUF_SIZE);
-        write(1, buf, BUF_SIZE);
-        write(fd, buf, BUF_SIZE);
+        bytes = read(STDIN_FILENO, buf, BUF_SIZE);
+
+        if (bytes < 0) {
+            perror("Error leyendo de entrada estándar");
+            return;
+        }
+
+        if (write(STDOUT_FILENO, buf, bytes) < 0) {
+            perror("Error escribiendo a salida estándar");
+            return;
+        }
+
+        if (write(fd, buf, bytes) < 0) {
+            perror("Error escribiendo al archivo de salida");
+            return;
+        }
+
     } while (bytes);
 
-    close(fd);
+    if (close(fd) < 0) {
+        perror("Error cerrando el archivo de salida");
+    }
 }
 
 int main(int argc, char** argv) {
-    teeo(argv[1]);
+    tee0(argv[1]);
 }
 
